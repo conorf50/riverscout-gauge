78729ed0cc1e12effe711970f5ed13bf593280b6
Conor <conorf50@gmail@.com>
2019-04-20 23:08:41 +0100

    remove while loop

--- main.py
+++ main.py
@@ -16,35 +16,34 @@ US100 distance sensor code forked from Kai Fricke (https://github.com/kfricke/) 
 Modified code available at: https://github.com/conorf50/pycom-us100
 '''
 
-#from network import Sigfox # sigfox libs
+from network import Sigfox # sigfox libs
 import time 
 from machine import Pin # listings for device pins
 from onewire import DS18X20 # driver for the temperature sensor
 from onewire import OneWire # onewire driver for DS18B20 driver above
-# import socket # sigfox
-# import struct # compression of float values
+import socket # sigfox
+import struct # compression of float values
 import sys 
 
-# # init Sigfox for RCZ1 (Europe)
-# sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
-# # create a Sigfox socket
-# s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
-# # make the socket blocking
-# s.setblocking(True)
-# # configure it as uplink only
-# s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
+# init Sigfox for RCZ1 (Europe)
+sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
+# create a Sigfox socket
+s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
+# make the socket blocking
+s.setblocking(True)
+# configure it as uplink only
+s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 
 #DS18B20 temp sensor data line (yellow wire) connected to pin P10 (G17 on Expansion Board)
 ow = OneWire(Pin('P10'))
 temp = DS18X20(ow) 
-while True:
-    temp.start_conversion()
-    time.sleep(1)
-    print("Temp =")
-    print(temp.read_temp_async())
-    time.sleep(1)
-    # 'f' = float value
-    #s.send(struct.pack('f',float(12.3)) + struct.pack('i', int(1020)))
-    # s.send(struct.pack('f',float(34.134)) + bytes([12]))
-    print("sending data")
-    #s.send(struct.pack('f',float(34.42)) + bytes([12]))
+temp.start_conversion()
+time.sleep(1)
+print("Temp =")
+print(temp.read_temp_async())
+time.sleep(1)
+# 'f' = float value
+#s.send(struct.pack('f',float(12.3)) + struct.pack('i', int(1020)))
+# s.send(struct.pack('f',float(34.134)) + bytes([12]))
+print("sending data")
+#s.send(struct.pack('f',float(34.42)) + bytes([12]))
