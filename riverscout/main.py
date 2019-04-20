08d2336ec57dc1df895e0326a615ed31d875e0a8
Conor <conorf50@gmail@.com>
2019-04-20 21:26:24 +0100

    added comments

--- riverscout/main.py
+++ riverscout/main.py
@@ -1,9 +1,30 @@
-# based on Pycom example here:
-# https://github.com/pycom/pycom-libraries/blob/master/examples/sigfoxUplink/main.py
-from network import Sigfox
-import socket
-import struct
-import sys
+'''
+Riverscout Device Code.
+Author:  Conor Farrell (+ others where noted)
+
+
+Sigfox parts based on Pycom example here:
+https://github.com/pycom/pycom-libraries/blob/master/examples/sigfoxUplink/main.py
+
+DS18X20 code from Pycom available here:
+https://github.com/pycom/pycom-libraries/tree/master/examples/DS18X20
+
+Struct function for data compression based on code from Austin Spivey/Wia.io available here:
+https://dzone.com/articles/build-an-end-to-end-sigfox-gps-tracker-using-wia-a
+
+US100 distance sensor code forked from Kai Fricke (https://github.com/kfricke/) and adapted to Pycom by myself
+Modified code available at: https://github.com/conorf50/pycom-us100
+'''
+
+from network import Sigfox # sigfox libs
+import time 
+from machine import Pin # listings for device pins
+from onewire import DS18X20 # driver for the temperature sensor
+from onewire import OneWire # onewire driver for DS18B20 driver above
+import socket # sigfox
+import struct # compression of float values
+import sys 
+
 # init Sigfox for RCZ1 (Europe)
 sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
 
@@ -18,7 +39,7 @@ s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
  
 try:
     print("sending data")
-    # 'f' = float value, 'i' = unsigned integer
+    # 'f' = float value
     #s.send(struct.pack('f',float(12.3)) + struct.pack('i', int(1020)))
     s.send(struct.pack('f',float(34.134)) + bytes([12]))
 except Exception:
