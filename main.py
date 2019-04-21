1ef5be77d19aba91467e7924d22518ae321be181
Conor <conorf50@gmail@.com>
2019-04-21 01:32:45 +0100

    add ADC code

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ Modified code available at: https://github.com/conorf50/pycom-us100
 from network import Sigfox # sigfox libs
 import time 
 from machine import Pin # listings for device pins
+from machine import ADC # 'substitute' for a temp sensor by using a pot connected to the ADC
 from onewire import DS18X20 # driver for the temperature sensor
 from onewire import OneWire # onewire driver for DS18B20 driver above
 import socket # sigfox
@@ -33,14 +34,19 @@ s.setblocking(True)
 # configure it as uplink only
 s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 
+# init the ADC
+adc = ADC(0)
+adc_c = adc.channel(pin='P13')
 #DS18B20 temp sensor data line (yellow wire) connected to pin P8 (G15 on Expansion Board)
-ow = OneWire(Pin('P8'))
+#ow = OneWire(Pin('P8')) # breaks Sigfox connectivity
 # temp = DS18X20(ow) 
 # temp.start_conversion()
 # time.sleep(1)
 # temp = round(temp.read_temp_async(), 2)
 
-# print("Temp =")
+value = adc_c.value()
+print("Temp =")
+print(value)
 # print(float(temp))
 # time.sleep(1)
 # # 'f' = float value
@@ -48,4 +54,4 @@ ow = OneWire(Pin('P8'))
 print("sending data")
     # 'f' = float value, 'i' = unsigned integer
     #s.send(struct.pack('f',float(12.3)) + struct.pack('i', int(1020)))
-s.send(struct.pack('f',float(34.1)) + bytes([12]))
+#s.send(struct.pack('f',float(34.1)) + bytes([12]))
