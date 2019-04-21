38d73977597a318c5142b4d904c05f0a0b822e5c
Conor <conorf50@gmail@.com>
2019-04-21 16:04:38 +0100

    add garbage collectiing code, make socket non blocking

--- main.py
+++ main.py
@@ -28,13 +28,16 @@ from onewire import DS18X20 # driver for the temperature sensor
 from onewire import OneWire # onewire driver for DS18B20 driver above
 import socket # sigfox
 import struct # compression of float values
+import gc
+# garbage collection
+gc.enable()
 
 # init Sigfox for RCZ1 (Europe)
 sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
 # create a Sigfox socket
 s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
 # make the socket blocking
-s.setblocking(True)
+s.setblocking(False)
 # configure it as uplink only
 s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 
@@ -52,28 +55,40 @@ s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 uart1 = machine.UART(1, baudrate=9600)
 sensor = us100.US100UART(uart1)
 
-
+print("Free memory pre sensor reading")
+print(str(gc.mem_free()))
 #DS18B20 temp sensor data line (yellow wire) connected to pin P8 (G15 on Expansion Board)
 ow = OneWire(Pin('P8')) # breaks Sigfox connectivity
 temp = DS18X20(ow) 
 temp.start_conversion()
 time.sleep(1)
 temp = round(temp.read_temp_async(), 2)
-
+print("Free memory post sensor reading")
+print(str(gc.mem_free()))
+gc.collect()
+print("Free memory post gc")
+print(str(gc.mem_free()))
 # take the value from the ADC, divide it by 100 to get a float value and round this to two decimal places
 #tempVal = round(adc_c.value()/100, 2)
 
 # get the distance in millimeters from the sensor
-#waterLevel = sensor.distance()
+waterLevel = sensor.distance()
 
 print("Temp =")
-#print(tempVal) # scale the value down to a more realistic level
+# #print(tempVal) # scale the value down to a more realistic level
 print(float(temp))
 print("Water Level = ")
 print(sensor.distance())
 # # 'f' = float value
 #adc.deinit()
 print("sending data")
+gc.collect()
+print("Free memory after data collcted")
+print(str(gc.mem_free()))
     # 'f' = float value, 'i' = unsigned integer
     #s.send(struct.pack('f',float(12.3)) + struct.pack('i', int(1020)))
-#s.send(struct.pack('f',float(temp)) + bytes([12]))
+try:
+    s.send(struct.pack('f',float(temp)) + bytes([123]))
+except Exception as error:
+    print("unable to send data" + str(error))
+    
