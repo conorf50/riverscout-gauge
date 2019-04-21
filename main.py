2d1c9db1b85f973241510b307c9c1e3ca86d4ad7
Conor <conorf50@gmail@.com>
2019-04-21 16:22:23 +0100

    remove ADC code

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ https://dzone.com/articles/build-an-end-to-end-sigfox-gps-tracker-using-wia-a
 US100 distance sensor code forked from Kai Fricke (https://github.com/kfricke/) and adapted to Pycom by myself
 Modified code available at: https://github.com/conorf50/pycom-us100
 '''
-
 from network import Sigfox # sigfox libs
 import time 
 from machine import UART
@@ -23,14 +22,11 @@ import utime
 import us100
 import pycom
 from machine import Pin # listings for device pins
-from machine import ADC # 'substitute' for a temp sensor by using a pot connected to the ADC
 from onewire import DS18X20 # driver for the temperature sensor
 from onewire import OneWire # onewire driver for DS18B20 driver above
 import socket # sigfox
 import struct # compression of float values
-import gc
-# garbage collection
-gc.enable()
+
 
 # init Sigfox for RCZ1 (Europe)
 sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
@@ -41,12 +37,6 @@ s.setblocking(False)
 # configure it as uplink only
 s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 
-# init the ADC
-#adc = ADC(0)
-# set up the ADC on pin 13 with an 11db attenuation factor 
-# this means the max value will be 4095 at 3.3v
-#adc_c = adc.channel(pin='P13', attn=ADC.ATTN_11DB)
-
 
 # The SiPy has two serial ports, one is used to communicate with the computer
 # the second is used here
@@ -55,21 +45,13 @@ s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 uart1 = machine.UART(1, baudrate=9600)
 sensor = us100.US100UART(uart1)
 
-print("Free memory pre sensor reading")
-print(str(gc.mem_free()))
+
 #DS18B20 temp sensor data line (yellow wire) connected to pin P8 (G15 on Expansion Board)
 ow = OneWire(Pin('P8')) # breaks Sigfox connectivity
 temp = DS18X20(ow) 
 temp.start_conversion()
 time.sleep(1)
 temp = round(temp.read_temp_async(), 2)
-print("Free memory post sensor reading")
-print(str(gc.mem_free()))
-gc.collect()
-print("Free memory post gc")
-print(str(gc.mem_free()))
-# take the value from the ADC, divide it by 100 to get a float value and round this to two decimal places
-#tempVal = round(adc_c.value()/100, 2)
 
 # get the distance in millimeters from the sensor
 waterLevel = sensor.distance()
@@ -80,15 +62,12 @@ print(float(temp))
 print("Water Level = ")
 print(sensor.distance())
 # # 'f' = float value
-#adc.deinit()
-print("sending data")
-gc.collect()
-print("Free memory after data collcted")
-print(str(gc.mem_free()))
-    # 'f' = float value, 'i' = unsigned integer
-    #s.send(struct.pack('f',float(12.3)) + struct.pack('i', int(1020)))
+
+#s.send(struct.pack('f',float(12.3)) + struct.pack('i', int(1020)))
 try:
-    s.send(struct.pack('f',float(temp)) + bytes([123]))
+    print("sending data")
+    # 'f' = float value, 'i' = unsigned integer
+    #s.send(struct.pack('f',float(temp)) + bytes([123]))
 except Exception as error:
     print("unable to send data" + str(error))
     
